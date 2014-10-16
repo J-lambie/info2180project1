c4231ef6610d6d5321bd9ecc218d4bc4fc8d6de5
J-lambie <kimmy_jill@hotmail.com>
2014-10-16 16:22:44 -0500

    Saving files

---
+++ Script.js
@@ -0,0 +1,114 @@
+/*Address Book*/
+
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    return (person.firstName + " " + person.lastName +"<li>"+" Phone: " + person.phoneNumber+"</li>" +"<li>"+ " Email: "+person.email+"</li>");
+}
+
+function list() {
+	var contactsLength = contacts.length;
+    var holder = [];
+	for (var i = 0; i < contactsLength; i++) {
+		holder[i] = printPerson(contacts[i])+"</br>";
+	}
+    return holder;
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+function search(lastName){
+    var contactsLength=contacts.length;
+    for(var i=0;i<contactsLength;i++){
+        if(contacts[i].lastName===lastName)
+            printPerson(contacts[i]);
+    }
+}
+search("Jones");
+function add(firstName,lastName,email,phoneNumber)
+{
+    
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.email=email;
+    this.phoneNumber=phoneNumber;
+    
+    }
+
+contacts[2]= new add("Jill","Joe","jill.joe@example.com","650 555-5555");
+list();
+
+
+
+
+/*
+Cash Register*/
+function StaffMember(name,discountPercent){
+    this.name = name;
+    this.discountPercent = discountPercent;
+}
+
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
+
+// Create yourself again as 'me' with a staff discount of 20%
+var me =new StaffMember("JS Ninja",20);
+
+var cashRegister = {
+    total:0,
+    lastTransactionAmount: 0,
+    add: function(itemCost){
+        this.total += (itemCost || 0);
+        this.lastTransactionAmount = itemCost;
+    },
+    scan: function(item,quantity){
+        switch (item){
+        case "eggs": this.add(0.98 * quantity); break;
+        case "milk": this.add(1.23 * quantity); break;
+        case "magazine": this.add(4.99 * quantity); break;
+        case "chocolate": this.add(0.45 * quantity); break;
+        }
+        return true;
+    },
+    voidLastTransaction : function(){
+        this.total -= this.lastTransactionAmount;
+        this.lastTransactionAmount = 0;
+    },
+    // Create a new method applyStaffDiscount here
+    applyStaffDiscount: function(employee){
+        var hold =(employee.discountPercent/100);
+        this.total-=(hold*this.total);
+    } 
+};
+
+cashRegister.scan('eggs',1);
+cashRegister.scan('milk',1);
+cashRegister.scan('magazine',3);
+// Apply your staff discount by passing the 'me' object 
+// to applyStaffDiscount
+cashRegister.applyStaffDiscount(me);
+
+// Show the total bill
+console.log('Your bill is '+cashRegister.total.toFixed(2));
+
+//Displaying Data
+window.onload =function(){
+    
+    document.getElementById('book').innerHTML =list();
+    document.getElementById('cashier').innerHTML=('Your bill is '+cashRegister.total.toFixed(2));
+   
+}
